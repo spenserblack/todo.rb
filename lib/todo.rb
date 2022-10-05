4a495a91ce685aa2c42edce07d992b2ed4f94543
Spenser Black <spenserblack01@gmail.com>
2022-10-05 19:52:42 -0400

    Add lib for completing items

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
-def add_2(n)
-  n + 2
+class Todo
+  # TodoList represents a list of items to do, and those that are done.
+  class TodoList
+    attr_reader :items, :completed_items
+
+    def initialize(items, completed_items = [])
+      @items = items
+      @completed_items = completed_items
+    end
+
+    # Completes one or more items, returning a new TodoList.
+    def complete(item_index, *other_item_indexes)
+      indices = Set.new([item_index, *other_item_indexes])
+      new_completed_items = @completed_items.dup
+      new_items = items.filter.with_index do |item, i|
+        includes = indices.include?(i)
+        new_completed_items << item if includes
+        !includes
+      end
+
+      new(new_items, new_completed_items)
+    end
+
+    # Completes one or more items.
+    def complete!(item_index, *other_item_indices)
+      [item_index, *other_item_indices].sort.reverse_each { |index| @completed_items << @items.delete_at(index) }
+    end
+  end
 end
