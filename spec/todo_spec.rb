699484bffdbe3da8ec17e81b0a077e2a32af1fea
Spenser Black <spenserblack01@gmail.com>
2022-10-08 17:15:58 -0400

    Test complete!

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -10,3 +10,14 @@ RSpec.describe Todo::TodoList, '#complete' do
     end
   end
 end
+
+RSpec.describe Todo::TodoList, '#complete!' do
+  context 'when there are 3 items and 3 completed items' do
+    it 'should return a new Todo with the middle item moved to the last completed item' do
+      todo = Todo::TodoList.new %w[a b c], %w[x y z]
+      todo.complete! 1
+      expect(todo.items).to eq %w[a c]
+      expect(todo.completed_items).to eq %w[x y z b]
+    end
+  end
+end
