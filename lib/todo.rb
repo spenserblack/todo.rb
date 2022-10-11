86782efff3a7816c3ac19ce525297b2977a8f6c6
Spenser Black <spenserblack01@gmail.com>
2022-10-11 18:32:15 -0400

    Add functions to read/write todo lists

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
+require 'yaml'
 
 class Todo
+  HOME = ENV['HOME']
+
   # TodoList represents a list of items to do, and those that are done.
   class TodoList
     attr_reader :items, :completed_items
@@ -27,5 +30,30 @@ class Todo
     def complete!(item_index, *other_item_indices)
       [item_index, *other_item_indices].sort.reverse_each { |index| @completed_items << @items.delete_at(index) }
     end
+
+    def to_h
+      { todo: items, done: completed_items }
+    end
+  end
+
+  def todo_dir
+    File.join HOME, '.todo.rb'
+  end
+
+  def filename(name)
+    File.join todo_dir, "#{name}.yaml"
+  end
+
+  def load_list(name)
+    Dir.mkdir tod_dir unless Dir.exist? todo_dir
+    unless File.exist? filename(name)
+      File.write filename(name), YAML.dump(TodoList.new([]).to_h)
+    end
+    list = YAML.load_file(filename(name))
+    TodoList.new(list[:todo] || [], list[:done] || [])
+  end
+
+  def save_list(name, list)
+    YAML.dump(list.to_h, File.open(filename(name), 'w'))
   end
 end
