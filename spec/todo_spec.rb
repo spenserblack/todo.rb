8d905ddd4994bd54b07d93b00dc16e5206ea6149
Spenser Black <spenserblack01@gmail.com>
2022-10-14 09:40:33 -0400

    Add tests

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -48,14 +48,25 @@ RSpec.describe Todo do
     end
   end
 
-  describe '#find_lists' do
+  describe Todo::TodoDir, '#list_path' do
+    context 'when dir is /tmp/todo' do
+      dir = described_class.new '/tmp/todo'
+
+      it 'returns /tmp/todo/bar' do
+        expect(dir.list_path('bar')).to eq '/tmp/todo/bar.yaml'
+      end
+    end
+  end
+
+  describe Todo::TodoDir, '#find_lists' do
     context 'when "foo.yaml" and "bar.yaml" exist' do
+      dir = described_class.new '.'
       before do
         allow(Dir).to receive(:glob).and_return %w[foo.yaml bar.yaml]
       end
 
       it 'returns ["foo", "bar"]' do
-        expect(described_class.find_lists).to eq %w[foo bar]
+        expect(dir.find_lists).to eq %w[foo bar]
       end
     end
   end
